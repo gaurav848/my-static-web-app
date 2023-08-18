12f74d40825aa2142a64ea7d56502feaacfd043b
Gaurav Gupta <Gaurav.gupta@lubrizol.com>
2023-08-18 14:20:33 -0400

    test change

--- angular-app/src/app/about.component.ts
+++ angular-app/src/app/about.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
       <div class="content-title-group not-found">
         <h2 class="title">Product Wish List</h2>
         <p>
+        Jai shri Harivansh.
           This project was created to help represent a fundamental app written
           with Angular. The shopping theme is used throughout the app.
         </p>
